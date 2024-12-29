548308168458aa936d17dc441ed20a2e0b2aa6c4
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2024-12-29 19:06:24 +0900

    test

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,9 +1,8 @@
-import rclpy
+mport rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import datetime
 import random
-
+import datetime
 
 class Talker(Node):
     def __init__(self):
@@ -17,13 +16,17 @@ class Talker(Node):
             "大腿二頭筋", "下腿三頭筋", "三角筋", "上腕二頭筋",
             "腹直筋", "大腿四頭筋"
         ]
-        selected_muscle = random.choice(muscle_groups)
-        today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        msg = String()
-        msg.data = f"日時: {today} | 今日の筋トレ部位: {selected_muscle}"
-        self.get_logger().info(f"Talker: {msg.data}")
-        self.pub.publish(msg)
+        
+        today = datetime.datetime.now().strftime("%Y-%m-%d")
 
+        selected_muscle = random.choice(muscle_groups)
+
+        msg = String()
+        msg.data = f"日付: {today} | 今日の筋トレ部位: {selected_muscle}"
+
+        self.get_logger().info(f"Talker: {msg.data}")
+        
+        self.pub.publish(msg)
 
 def main():
     rclpy.init()
