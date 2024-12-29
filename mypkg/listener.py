f0fb83a79d1eed8aff8b34b5696bef93ff28b0ae
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2024-12-29 20:12:51 +0900

    test

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,17 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import String
 
 
-rclpy.init()
-node = Node("listener")
+class Listener(Node):
+    def __init__(self):
+        super().__init__("listener")
+        self.sub = self.create_subscription(String, "question", self.cb, 10)
 
-
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+    def cb(self, msg):
+        # 受信したメッセージをログに出力
+        self.get_logger().info(f"Listener received: {msg.data}")
 
 
 def main():
-    sub = node.create_subscription(Int16, "countup", cb, 10)
+    rclpy.init()
+    node = Listener()
     rclpy.spin(node)
