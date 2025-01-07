6257c1c5d41757ea3c972a8ce61b440241f05a26
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2025-01-08 00:16:09 +0900

    workout

--- test/test.bash
+++ test/test.bash
@@ -6,17 +6,22 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
-
 colcon build
 source $dir/.bashrc
 source /opt/ros/humble/setup.bash
 
-timeout 10 ros2 run mypkg talker > /tmp/mypkg_talker.log
+timeout 10 ros2 run mypkg workout_publisher &
 
-tail -n 20 /tmp/mypkg_talker.log
+sleep 3
 
-if grep -q "今日の筋トレ部位" /tmp/mypkg_talker.log; then
-  echo "Talkerノードが有用なデータを発信しています。成功です。"
+topic_name="workout_plan"
+log_file="/tmp/workout_plan.log"
+timeout 7 ros2 topic echo /$topic_name > $log_file
+
+if grep -q "筋トレ部位" $log_file && grep -q "トレーニング" $log_file; then
+  echo "トピック $topic_name に正しいデータが発信されています。成功です。"
 else
-  echo "Talkerノードが有用なデータを発信していません。エラーです。"
+  echo "トピック $topic_name に正しいデータが発信されていません。エラーです。"
 fi
+
+rm -f $log_file
